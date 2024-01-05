6e4aab47ed9f9bf546fd29dea0598f48329e383e
Sergey Pudov <pellexa@gmail.com>
2024-01-05 12:17:43 +0300

    feat(dark): add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,js}"];
 export const theme = {
-  extend: {},
+  screens: {
+    "2xl": "1440px", // => @media (min-width: 1440px) { ... }
+    lg: "1024px", // => @media (min-width: 1024px) { ... }
+    xs: "375px", // => @media (min-width: 375px) { ... }
+  },
+  extend: {
+    colors: {
+      "dark-color": "#07070A",
+      "accent-color": "#FAD007",
+    },
+    transitionProperty: {
+      "bg-color": "background-color",
+    },
+  },
 };
 export const plugins = [];
